f5b250a60b595625337d8c9755f272f299d8d33d
DiegoVillasenorVilla <divivi.619@gmail.com>
2022-03-14 17:20:29 -0600

    Primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect, useRef} from "react";
 
 function App() {
+
+  const [animal, setAnimal] = useState([]);
+  const [filter, setFilter] = useState('');
+  //const [counter, setCounter] = useState(0);
+  const TextInput = useRef();
+
+  const FiltrarDatos = animal.filter((item)=> item.name.toLowerCase().includes(filter));
+
+  useEffect(() => {
+    const fetchAnimals = async () =>{
+      const response = await fetch('https://zoo-animal-api.herokuapp.com/animals/rand/10');
+      const data = await response.json();
+      setAnimal(data);
+      TextInput.current.focus();
+    }
+    fetchAnimals();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Animales</h1>
+        <input ref={TextInput} placeholder="Search ..." onChange={(e) => setFilter(e.target.value)} />
+        {FiltrarDatos?.map((item,i)=>(
+           <p key={i}>{item.name}</p>
+        )) }
+        
       </header>
     </div>
   );
